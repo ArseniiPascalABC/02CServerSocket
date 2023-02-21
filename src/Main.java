e34afb27d4a0a9950711a64d9f608759c76c55a1
CEH9 <senya.porfirev@bk.ru>
2023-02-21 11:32:00 +0300

    second commit, hello user name + primer 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
         System.out.println("Hello " + args[0]);
+
+        System.out.println("Введите х: ");
+        int x = sc.nextInt();
+        System.out.println("Введите y: ");
+        int y = sc.nextInt();
+        double gg = ((3*Math.pow(Math.cos(x-(Math.PI/6)),2))/((1/2)+Math.sin(Math.pow(y,2))));
+        System.out.println("x = " + x +"\n" + "y = " + y +"\n" + "otvet = " + gg);
+
+
     }
 }
